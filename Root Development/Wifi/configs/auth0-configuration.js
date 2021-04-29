b5229097cb77d0d261a28b3e2ea548d04e4df98e
BrandonFeldhaus <59741125+BrandonFeldhaus@users.noreply.github.com>
2021-04-29 13:45:42 -0400

    Changing Auth0 key

--- Root Development/Wifi/configs/auth0-configuration.js
+++ Root Development/Wifi/configs/auth0-configuration.js
@@ -1,4 +1,4 @@
 module.exports = {
-    clientId: "2f4aVEfOwjpfYouktTo7LQFwCI0N69Ok",
-    domain: "dev-awc6c4u1.us.auth0.com"
+    clientId: "Enter your own Auth0 client id here",
+    domain: "Enter you Auth0 domain name here"
 };
